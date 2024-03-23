fa926ff7dcc62eeb23eab75edd99b6faea93d21a
JA-FSU <78230541+JA-FSU@users.noreply.github.com>
2024-03-23 12:34:52 -0400

    Setting up enemy AI

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -11,20 +11,65 @@ public class EnemyScript : MonoBehaviour
     public Transform[] navPoint;
 
     public int destPoint = 0;
-    public float enemySpeed = 5.0f;
+    public float enemySpeed = 20.0f;
     public float playerDistance;
-    public float awareAI = 10f;
+    public float awareAI = 0.1f;
     public float damping = 6.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        UnityEngine.AI.NavMeshAgent agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent.destination = goal.position;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        playerDistance = Vector3.Distance(Player.position, transform.position);
+
+        if (playerDistance < awareAI)
+        {
+            LookAtPlayer();
+            Debug.Log("Seen");
+
+            if (playerDistance < 2f)
+            {
+                Chase();
+            }
+        }
+        else
+        {
+            GotoNextPoint();
+        }
+
+
+        if (agent.remainingDistance < 0.5f)
+        {
+            GotoNextPoint();
+        }
+    }
+
+    void LookAtPlayer()
+    {
+        transform.LookAt(Player);
+    }
+
+
+    void GotoNextPoint()
+    {
+        if (navPoint.Length == 0)
+        {
+            return;
+        }
+        agent.destination = navPoint[destPoint].position;
+        destPoint = (destPoint + 1) % navPoint.Length;
+    }
+
+
+    void Chase()
+    {
+        transform.Translate(Vector3.forward * enemySpeed * Time.deltaTime);
     }
 }
