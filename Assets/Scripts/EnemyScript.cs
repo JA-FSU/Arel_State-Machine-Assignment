77c8c27c242eb1c3a8034787da5fbcf0f8ca7280
JA-FSU <78230541+JA-FSU@users.noreply.github.com>
2024-03-23 14:40:58 -0400

    Finished enemy AI

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class EnemyScript : MonoBehaviour
 {
     public UnityEngine.AI.NavMeshAgent agent;
+    private GameManager gameManager;
 
     public Transform Player;
     public Transform goal;
     public Transform[] navPoint;
 
-    public int destPoint = 0;
-    public float enemySpeed = 20.0f;
-    public float playerDistance;
-    public float awareAI = 0.1f;
-    public float damping = 6.0f;
+    private int destPoint = 0;
+    [SerializeField] private int randNum = 0;
+    public float enemySpeed;
+    private float playerDistance;
+    private float awareAI = 3f;
+    private bool isChasing = false;
+    private bool isLooking = false;
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +25,9 @@ public class EnemyScript : MonoBehaviour
         UnityEngine.AI.NavMeshAgent agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
         agent.destination = goal.position;
 
+        agent.autoBraking = false;
+
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
@@ -29,23 +35,32 @@ public class EnemyScript : MonoBehaviour
     {
         playerDistance = Vector3.Distance(Player.position, transform.position);
 
+        if (randNum != 10)
+        {
+            randNum = Random.Range(1, 500);
+        }
+
         if (playerDistance < awareAI)
         {
-            LookAtPlayer();
-            Debug.Log("Seen");
-
-            if (playerDistance < 2f)
+            if (playerDistance < awareAI)
             {
+                LookAtPlayer();
+                Debug.Log("Seen");
+                StopCoroutine("LookAround");
+                agent.isStopped = false;
                 Chase();
             }
+            else
+            {
+                GotoNextPoint();
+            }
         }
-        else
+
+        if (randNum == 10 && !isChasing && !isLooking)
         {
-            GotoNextPoint();
+            StartCoroutine("LookAround");
         }
-
-
-        if (agent.remainingDistance < 0.5f)
+        else if (agent.remainingDistance < 0.5f)
         {
             GotoNextPoint();
         }
@@ -70,6 +85,41 @@ public class EnemyScript : MonoBehaviour
 
     void Chase()
     {
+        isChasing = true;
         transform.Translate(Vector3.forward * enemySpeed * Time.deltaTime);
+
+        if (playerDistance > awareAI && gameManager.gameActive)
+        {
+            isChasing = false;
+            GotoNextPoint();
+        }
+    }
+
+    IEnumerator LookAround()
+    {
+        isLooking = true;
+        agent.isStopped = true;
+        int randTimes = Random.Range(1, 3);
+
+        float turnTime = 50.0f;
+        Quaternion targetO = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y+50.0f, transform.eulerAngles.z);
+        Quaternion targetT = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y-100.0f, transform.eulerAngles.z);
+
+
+        if (randTimes > 0)
+        {
+            yield return new WaitForSeconds(1);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetO, turnTime * Time.deltaTime);
+            yield return new WaitForSeconds(1);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetT, turnTime * Time.deltaTime);
+            yield return new WaitForSeconds(2);
+            transform.LookAt(agent.destination);
+
+            randTimes -= 1;
+        }
+        randNum = 0;
+        agent.isStopped = false;
+        isLooking = false;
+        StopCoroutine("LookAround");
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public bool gameActive = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameActive = true;
     }
 
     // Update is called once per frame
@@ -16,7 +19,14 @@ public class GameManager : MonoBehaviour
         if (Input.GetKey(KeyCode.Escape))
         {
             Debug.Log("Quitting game...");
+            gameActive = false;
             Application.Quit();
+        }
+
+        if (Input.GetKey(KeyCode.R))
+        {
+            Time.timeScale = 1.0f;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
 }
